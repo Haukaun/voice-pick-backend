94c8126a50c1e3f3ed80f46a2b8dcd6f85d235f4
tama3ti <m.picheta@icloud.com>
2023-02-10 13:01:46 +0100

    initial commit

---
+++ src/main/java/no/ntnu/bachelor/voicepick/VoicePickApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.bachelor.voicepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoicePickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoicePickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/bachelor/voicepick/VoicePickApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.bachelor.voicepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicePickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
