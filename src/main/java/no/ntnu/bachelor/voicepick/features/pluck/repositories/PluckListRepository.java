bf5dbaf0f334ebe38d37833dfa6a1350135b75f0
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-05-04 15:12:46 +0200

    endpoint for getting number of pluck lists

--- src/main/java/no/ntnu/bachelor/voicepick/features/pluck/repositories/PluckListRepository.java
+++ src/main/java/no/ntnu/bachelor/voicepick/features/pluck/repositories/PluckListRepository.java
@@ -5,6 +5,8 @@ import no.ntnu.bachelor.voicepick.models.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import no.ntnu.bachelor.voicepick.features.pluck.models.PluckList;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,5 +16,7 @@ public interface PluckListRepository extends JpaRepository<PluckList, Long> {
   List<PluckList> findByUser(User user);
   Optional<PluckList> findByIdAndWarehouse(Long id, Warehouse warehouse);
 
+  @Query("SELECT COUNT(PluckList) FROM PluckList p WHERE p.finishedAt IS NOT NULL AND p.user.uuid = :uuid")
+  Integer countCompletedPluckList(@Param("uuid") String uuid);
 
 }
